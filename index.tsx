f345a837fb072e8016c26ecca09029caf3952c42
ck77 <kuo.sunnyfruit@gmail.com>
2022-03-25 10:02:38 +0800

    init

---
+++ index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+// import App from './src/form/App';
+// import App from './src/pattern1/App';
+// import App from './src/state/App';
+// import App from './src/decorator/App';
+// import App from './src/ref/App';
+// import App from './src/ref/demo3/App';
+// import App from './src/mobx-demo1/App';
+// import App from './src/mobx-react/basic2/App';
+// import App from './src/context/App';
+// import App from './src/ngm/App';
+// import App from './src/ngm-product/App-hooks';
+import App from './src/app';
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("example")
+);
